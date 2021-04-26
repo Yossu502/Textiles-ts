2dd939bf5bd787fc113e634bfac38cb676b57033
Josué Méndez <42630284+Yossu502@users.noreply.github.com>
2021-04-26 00:07:39 -0600

    inicio sesion

---
+++ software Textiles-ts/src/forms/inicioSesion.java
@@ -0,0 +1,320 @@
+package forms;
+
+import clases.ConexionDB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TimerTask;
+import javax.swing.JOptionPane;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class inicioSesion extends javax.swing.JFrame {
+
+    public inicioSesion() {
+        initComponents();
+        setLocationRelativeTo(null);
+        this.setTitle("Software Textiles TS");
+        this.setResizable(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnl_bg = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        login = new javax.swing.JPanel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
+        label_user = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        btt_inicio = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        label_password = new javax.swing.JPasswordField();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        loader = new javax.swing.JPanel();
+        img_loader = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(730, 410));
+
+        pnl_bg.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new java.awt.CardLayout());
+
+        login.setBackground(new java.awt.Color(255, 255, 255));
+
+        jSeparator1.setBackground(new java.awt.Color(41, 168, 73));
+        jSeparator1.setForeground(new java.awt.Color(41, 168, 73));
+
+        jSeparator2.setBackground(new java.awt.Color(41, 168, 73));
+        jSeparator2.setForeground(new java.awt.Color(41, 168, 73));
+
+        label_user.setBackground(new java.awt.Color(255, 255, 255));
+        label_user.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        label_user.setForeground(new java.awt.Color(204, 204, 204));
+        label_user.setText("Usuario");
+        label_user.setBorder(null);
+        label_user.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                label_userFocusGained(evt);
+            }
+        });
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/user.png"))); // NOI18N
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/unlock.png"))); // NOI18N
+
+        btt_inicio.setBackground(new java.awt.Color(41, 168, 73));
+        btt_inicio.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btt_inicio.setForeground(new java.awt.Color(255, 255, 255));
+        btt_inicio.setText("Iniciar Sesión");
+        btt_inicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btt_inicioActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI Semilight", 0, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel4.setText("¿Has olvidado tu contraseña?");
+
+        label_password.setBackground(new java.awt.Color(255, 255, 255));
+        label_password.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        label_password.setForeground(new java.awt.Color(204, 204, 204));
+        label_password.setText("Contraseña");
+        label_password.setBorder(null);
+        label_password.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                label_passwordFocusGained(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel5.setText("Iniciar Sesión");
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI Semilight", 0, 12)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel6.setText("Crear nuevo usuario");
+
+        javax.swing.GroupLayout loginLayout = new javax.swing.GroupLayout(login);
+        login.setLayout(loginLayout);
+        loginLayout.setHorizontalGroup(
+            loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginLayout.createSequentialGroup()
+                .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(loginLayout.createSequentialGroup()
+                        .addGap(207, 207, 207)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                        .addComponent(btt_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(loginLayout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 29, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(label_user, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(label_password, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(201, 201, 201))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel6)
+                .addContainerGap())
+            .addGroup(loginLayout.createSequentialGroup()
+                .addGap(186, 186, 186)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        loginLayout.setVerticalGroup(
+            loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, loginLayout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(loginLayout.createSequentialGroup()
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(label_user, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(loginLayout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(label_password, javax.swing.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE))
+                    .addGroup(loginLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btt_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addGap(43, 43, 43)
+                .addComponent(jLabel6)
+                .addGap(14, 14, 14))
+        );
+
+        jPanel1.add(login, "card2");
+
+        loader.setBackground(new java.awt.Color(255, 255, 255));
+
+        img_loader.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/load.gif"))); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(41, 168, 73));
+        jLabel1.setText("Cargando...");
+
+        javax.swing.GroupLayout loaderLayout = new javax.swing.GroupLayout(loader);
+        loader.setLayout(loaderLayout);
+        loaderLayout.setHorizontalGroup(
+            loaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(loaderLayout.createSequentialGroup()
+                .addGroup(loaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(loaderLayout.createSequentialGroup()
+                        .addGap(302, 302, 302)
+                        .addComponent(img_loader))
+                    .addGroup(loaderLayout.createSequentialGroup()
+                        .addGap(316, 316, 316)
+                        .addComponent(jLabel1)))
+                .addContainerGap(308, Short.MAX_VALUE))
+        );
+        loaderLayout.setVerticalGroup(
+            loaderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(loaderLayout.createSequentialGroup()
+                .addGap(141, 141, 141)
+                .addComponent(img_loader, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addContainerGap(81, Short.MAX_VALUE))
+        );
+
+        jPanel1.add(loader, "card3");
+
+        javax.swing.GroupLayout pnl_bgLayout = new javax.swing.GroupLayout(pnl_bg);
+        pnl_bg.setLayout(pnl_bgLayout);
+        pnl_bgLayout.setHorizontalGroup(
+            pnl_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        pnl_bgLayout.setVerticalGroup(
+            pnl_bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnl_bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnl_bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btt_inicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btt_inicioActionPerformed
+        // TODO add your handling code here:
+        //verificar aun el usuario y contraseña
+        String user = label_user.getText();
+        String pass = label_password.getText();
+        String pass1 = Encriptado(pass);
+        if(Verificar(user, pass1) == true){
+            loader.show();
+            login.hide();
+            new java.util.Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    menuPrincipal ventana = new menuPrincipal();
+                    ventana.show();
+                    dispose();
+                }
+            }, 1000);
+        }else{
+            JOptionPane.showMessageDialog(null, "Usuario o contraseña no validos", "Error", JOptionPane.ERROR_MESSAGE);
+        } 
+    }//GEN-LAST:event_btt_inicioActionPerformed
+    
+    public String Encriptado(String pass){
+        String encript=DigestUtils.sha512Hex(pass);
+        return encript;
+    }
+    
+    public Boolean Verificar(String user, String pass){
+        String dts[] = new String[2];
+        boolean resultado = false;
+        try {
+            Connection con1 = null;
+            ConexionDB conect1 = new ConexionDB();
+            con1 = conect1.conectarMySQL();
+            String sql = "SELECT `user`, `password` FROM `prueba`.`usuarios` WHERE  `user`="+"'"+user+"'" +" AND `password`="+"'"+pass+"'";
+            Statement st = con1.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                if(rs.getString("user").equals(user) && rs.getString("password").equals(pass)){
+                    resultado = true;
+                }else{
+                    resultado = false;
+                }
+            }
+            conect1.Desconectar();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "SQL NO COMPLETADA", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return resultado;
+    }
+    
+    private void label_userFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_label_userFocusGained
+        // TODO add your handling code here:
+        label_user.setText("");
+    }//GEN-LAST:event_label_userFocusGained
+
+    private void label_passwordFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_label_passwordFocusGained
+        // TODO add your handling code here:
+        label_password.setText("");
+    }//GEN-LAST:event_label_passwordFocusGained
+
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new inicioSesion().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btt_inicio;
+    private javax.swing.JLabel img_loader;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JPasswordField label_password;
+    private javax.swing.JTextField label_user;
+    private javax.swing.JPanel loader;
+    private javax.swing.JPanel login;
+    private javax.swing.JPanel pnl_bg;
+    // End of variables declaration//GEN-END:variables
+}
